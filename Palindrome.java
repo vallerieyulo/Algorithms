d59576e0b1751747083f0fa553a7300599cd44ae
Vallerie <valleriee.yulo@gmail.com>
2024-04-25 14:33:00 +0800

    Updated palindrome algorithm, second commit

--- Palindrome.java
+++ Palindrome.java
@@ -1,24 +1,31 @@
 public class Palindrome {
 
+    public static void main(String[] args) {
+
+        String phrase = "A man, a plan, a canal: Panama";
+
+        System.out.println("Phrase: " + phrase);
+        System.out.println("Is it a palindrome? " + isPalindrome(phrase));
+       // System.out.println("Reversed: " + reverse(phrase));
+    }
+    
     public static boolean isPalindrome(String phrase) {
 
         String cleanedPhrase = phrase.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        System.out.println(cleanedPhrase);
+        //System.out.println(cleanedPhrase);
 
         int start = 0;
         int end = cleanedPhrase.length() - 1;
         String reversed = "";
 
         while (start < end) {
-            reversed = reversed + cleanedPhrase.charAt(end);
-            System.out.println("reversed: " + reversed);
+            reversed += cleanedPhrase.charAt(end);
 
             if (cleanedPhrase.charAt(start) != cleanedPhrase.charAt(end)) {
                 return false;
             }
             start++;
             end--;
-
         }
 
         return true;
@@ -27,21 +34,14 @@ public class Palindrome {
     public static String reverse(String phrase) {
         String reversed = "";
         String cleanedPhrase = phrase.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        System.out.println("Clean: " + cleanedPhrase);
+        //System.out.println("Clean: " + cleanedPhrase);
         
         for (int i = cleanedPhrase.length() - 1; i >= 0; i--) {
-            reversed = reversed + cleanedPhrase.charAt(i);
+            reversed += cleanedPhrase.charAt(i);
         }
 
         return reversed;
     }
 
-    public static void main(String[] args) {
-
-        String phrase = "A man, a plan, a canal: Panama";
-        System.out.println("Phrase: " + phrase);
-        // System.out.println("Is it a palindrome? " + isPalindrome(phrase));
-
-        System.out.println("Reversed: " + reverse(phrase));
-    }
+  
 }
